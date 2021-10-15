8dc76757bf7f95e31f70cf73d8cc672b76672417
danielleleigh <danielle.leigh90@gmail.com>
2021-10-15 17:07:04 -0500

    added current errors found by debugging to file as comment so we know where we left off

--- ai.py
+++ ai.py
@@ -4,3 +4,10 @@ class AI(Player):
     def __init__(self):
         self.human = False
 
+AI(Player)
+""" Exception has occurred: TypeError       (note: full exception trace is shown but execution is paused at: <module>)
+__init__() takes 1 positional argument but 2 were given
+  File "/Users/danielle/Desktop/DevCodeCamp/rpsls/ai.py", line 7, in <module> (Current frame)
+    AI(Player)
+  File "/Users/danielle/Desktop/DevCodeCamp/rpsls/main.py", line 41, in <module>
+    from ai import AI """
