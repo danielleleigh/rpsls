fb85bd5cf8ee949e6e0a802aab7db53231dc052a
Your Name <charles.luas.young@gmail.com>
2021-10-15 14:03:41 -0600

    Outline added

---
+++ outline.py
@@ -0,0 +1,62 @@
+"""
+RPSLS
+
+---User experience-------------
+
+1. Loading....
+2. Welcome screen
+    Title RPSLS
+    best 2 out of 3
+    Display Rules
+3. Choose game
+    Single vs AI 
+    PvP
+4. Names
+    Player 1
+    Player 2
+5. Game begins
+6. Choose move
+7. Display match winner
+    Win count
+    If draw go back to 6.
+8. If best 2 out of 3 display game winner
+9. Ask to play again
+
+------Code it out!------------------
+
+main.py---------------
+
+variable:
+ Scissors
+ Rock
+ Paper
+ Spock
+ Lizard
+ Bad input
+ User 1
+ User 2
+ AI
+ Victor
+ best of 3
+
+def choose_game()
+
+
+Battlefield----------------------
+
+
+
+
+Class Player--------------------
+
+    Class AI----------------------
+
+    Class Human--------------------
+
+Class List_Gestures.py-------------
+options list
+Gestures
+
+
+
+"""
