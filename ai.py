0de3ce122f217799e680e734bd633c408afd4fa1
danielleleigh <danielle.leigh90@gmail.com>
2021-10-18 10:05:14 -0500

    added super function to AI child class

--- ai.py
+++ ai.py
@@ -3,11 +3,6 @@ from player import Player
 class AI(Player):
     def __init__(self):
         self.human = False
+        super().__init__()
 
-AI(Player)
-""" Exception has occurred: TypeError       (note: full exception trace is shown but execution is paused at: <module>)
-__init__() takes 1 positional argument but 2 were given
-  File "/Users/danielle/Desktop/DevCodeCamp/rpsls/ai.py", line 7, in <module> (Current frame)
-    AI(Player)
-  File "/Users/danielle/Desktop/DevCodeCamp/rpsls/main.py", line 41, in <module>
-    from ai import AI """
+
